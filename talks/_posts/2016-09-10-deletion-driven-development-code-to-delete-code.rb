21ae1cd1916a7f90f47e27a3ebe29b16d9a39da5
Chris Arcand <chris@chrisarcand.com>
2016-09-09 15:54:56 +0900

    DDD slides stub

---
+++ talks/_posts/2016-09-10-deletion-driven-development-code-to-delete-code.rb
@@ -0,0 +1,20 @@
+---
+  layout: talk
+  title: "Deletion Driven Development: Code to delete code!"
+  presented_to: "RubyKaigi 2016
+  tags: [Talks, Ruby]
+---
+
+Presented at RubyKaigi 2016 in Kyoto, Japan.
+
+Slides will be available after my presentation!
+
+### Abstract
+
+Good news! Ruby is a successful and mature programming language with a wealth
+of libraries and legacy applications that have been contributed to for many
+years. The bad news: Those projects might contain a large amount of useless,
+unused code which adds needless complexity and confuses new developers. In this
+talk I'll explain how to build a static analysis tool to help you clear out the
+cruft - because there's no code that's easier to maintain than no code at all!
+
